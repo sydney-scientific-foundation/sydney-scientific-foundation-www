8ca095c083a6b84bce437067b1fa44852f6aef95
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-07-30 18:32:20 +1000

    Experiment with a new feature section on the homepage

--- src/app/slash/slash.component.ts
+++ src/app/slash/slash.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 import { SiteModeService } from '../site-mode.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 
 @Component({
@@ -14,11 +15,19 @@ export class SlashComponent implements OnInit {
   videoNumber: string;
 
   constructor(private sanitizer: DomSanitizer,
-              public siteModeService: SiteModeService) {
+              public siteModeService: SiteModeService,
+              private snackBar: MatSnackBar) {
     this.videoNumber = '287747';
     this.safeHtml = sanitizer.bypassSecurityTrustHtml('<iframe style="width: 100%; height: 500px" allowfullscreen="true" webkitallowfullscreen="true" mozallowfullscreen="true" src="https://asciinema.org/a/' + this.videoNumber + '/iframe"></iframe>');
   }
 
   ngOnInit() {
+  }
+
+  coming_soon() {
+    this.snackBar.open('Coming soon!', null,
+      {
+        duration: 2500,
+      });
   }
 }
--- src/app/slash/slash.module.ts
+++ src/app/slash/slash.module.ts
@@ -4,6 +4,10 @@ import { RouterModule } from '@angular/router';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
 
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
 import { SvgViewerModule } from '../svg-viewer/svg-viewer.module';
 import { SlashComponent } from './slash.component';
 import { slashRoutes } from './slash.routes';
@@ -13,7 +17,7 @@ import { NavbarModule } from '../navbar/navbar.module';
   declarations: [SlashComponent],
   imports: [
     CommonModule, RouterModule, RouterModule.forChild(slashRoutes),
-    FlexLayoutModule, SvgViewerModule, NavbarModule
+    FlexLayoutModule, SvgViewerModule, NavbarModule, MatCardModule, MatButtonModule, MatSnackBarModule
   ]
 })
 export class SlashModule {}
