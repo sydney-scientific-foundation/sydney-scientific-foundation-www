1fa689d182aefa6029200405ce911f9746156cab
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2024-05-29 22:07:59 -0400

    Angular 18

--- src/app/svg-viewer/svg-viewer.module.ts
+++ src/app/svg-viewer/svg-viewer.module.ts
@@ -1,14 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import {SvgViewerComponent} from './svg-viewer.component';
 
-@NgModule({
-  exports: [SvgViewerComponent],
-  imports: [
-    CommonModule, HttpClientModule
-  ],
-  declarations: [SvgViewerComponent],
-})
+@NgModule({ exports: [SvgViewerComponent],
+    declarations: [SvgViewerComponent], imports: [CommonModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class SvgViewerModule { }
